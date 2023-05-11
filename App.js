32856a80ee56de09a827014bbec48976a833a411
Hasaranga-sys <65327930+Hasaranga-sys@users.noreply.github.com>
2023-05-11 09:41:30 +0530

    mobileApp

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import ClientHome from './screens/ClientHome';
+import AllBookingsList from './screens/AllBookingsList';
+import LoginPage from './screens/LoginPage';
 
+
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+     
+      <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Login' component={LoginPage}/>
+        <Stack.Screen name="ClientHome" component={ClientHome} />
+        <Stack.Screen name="AllBookingsList" component={AllBookingsList} />
+      </Stack.Navigator>
+      </NavigationContainer>
+        
+      
+      
     </View>
   );
 }
@@ -13,8 +30,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
+   
   },
 });
